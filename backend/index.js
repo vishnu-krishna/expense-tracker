24b5533f4fefed37125c9353532245fba71c5298
Vishnu <vbalakrishnan@seek.com.au>
2024-04-02 14:28:01 +1100

    authentication done.

--- backend/index.js
+++ backend/index.js
@@ -65,7 +65,7 @@ await server.start();
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
 app.use(
-    '/',
+    '/graphql',
     cors({
         origin: 'http://localhost:3000',
         credentials: true,
@@ -90,4 +90,4 @@ app.get('*', (req, res) => {
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
 
-console.log(`🚀 Server ready at http://localhost:4000/`);
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
